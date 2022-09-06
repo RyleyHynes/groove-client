e0e914cfa810fb34125e86338852cc2813ee088c
Ryley Hynes <ryleyhynes@gmail.com>
2022-09-06 14:03:51 -0500

    log in set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { Groove } from './Groove';
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById("root")
+const root = createRoot(container)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+        <Groove />
+    </BrowserRouter>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
