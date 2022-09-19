4cafef78a893a32bb686adbe53b61c5c9a2c2d7b
Ryley Hynes <ryleyhynes@gmail.com>
2022-09-19 17:54:40 -0500

    basic form styling

--- src/components/artists/EditArtistForm.js
+++ src/components/artists/EditArtistForm.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router"
 import { getSingleArtist, updateArtist } from "../managers/ArtistManager"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export const EditArtist = () => {
     //assigning the artist state to an object of key value pairs that are all set to empty strings
@@ -41,68 +43,52 @@ export const EditArtist = () => {
 
     //HTML form the user will see to update the artist
     return <>
-        <form className="artistForm">
+        <Form>
             <h2 className="updateArtist">Update Artist</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="artist_name" className="label">Artist Name:</label>
-                    <div className="control">
-                        <input className="input" required autoFocus
-                            type="text"
-                            value={artist.artist_name}
-                            name="artist_name"
-                            //When the value changes the changeArtistState function is triggered
-                            onChange={changeArtistState} />
-                    </div>
-                </div>
-            </fieldset>
+            <Form.Group className="mb-3" controlId="formBasicArtist">
+                <Form.Label>Artist Name:</Form.Label>
+                <Form.Control className="input" required autoFocus
+                    type="text"
+                    value={artist.artist_name}
+                    name="artist_name"
+                    //When the value changes the changeArtistState function is triggered
+                    onChange={changeArtistState} />
+            </Form.Group>
 
-            <fieldset>
-                <div className="form-group">
-                    <label>Genre:</label>
-                    <div className="control">
-                        <input className="input" required autoFocus
-                            type="text"
-                            value={artist.genre}
-                            name="genre"
-                            onChange={changeArtistState} />
-                    </div>
-                </div>
-            </fieldset>
+            <Form.Group className="mb-3" controlId="formBasicGenre">
+                <Form.Label>Genre:</Form.Label>
+                <Form.Control className="input" required autoFocus
+                    type="text"
+                    value={artist.genre}
+                    name="genre"
+                    onChange={changeArtistState} />
+            </Form.Group>
 
-            <fieldset>
-                <div className="form-group">
-                    <label>Description:</label>
-                    <div className="control">
-                        <input className="input" required autoFocus
-                            type="text"
-                            value={artist.artist_description}
-                            name="artist_description"
-                            onChange={changeArtistState} />
-                    </div>
-                </div>
-            </fieldset>
+            <Form.Group className="mb-3" controlId="formBasicDescription">
+                <Form.Label>Description:</Form.Label>
+                <Form.Control required autoFocus
+                    as="textarea" rows={2}
+                    value={artist.artist_description}
+                    name="artist_description"
+                    onChange={changeArtistState} />
+            </Form.Group>
 
-            <fieldset>
-                <div className="form-group">
-                    <label>URL Image:</label>
-                    <div className="control">
-                        <input className="input" required autoFocus
-                            type="text"
-                            value={artist.artist_image}
-                            name="artist_image"
-                            onChange={changeArtistState} />
-                    </div>
-                </div>
-            </fieldset>
-        
-            <button type="submit"
+            <Form.Group className="mb-3" controlId="formBasicURL">
+                <Form.Label>URL Image:</Form.Label>
+                <Form.Control className="input" required autoFocus
+                    type="text"
+                    value={artist.artist_image}
+                    name="artist_image"
+                    onChange={changeArtistState} />
+            </Form.Group>
+
+            <Button type="submit"
                 onClick={handleSubmit} //when save is clicked the handleSubmit function is triggered
                 className="button is-success">
                 Save
-            </button>
+            </Button>
             {/* when cancel is clicked it navigates the user back to the artist list */}
-            <button onClick={() => navigate("/artistList")}>Cancel</button>
-        </form>
+            <Button onClick={() => navigate("/artistList")}>Cancel</Button>
+        </Form>
     </>
 }
